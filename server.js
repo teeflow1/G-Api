fd6d664b27c27ab7c7760776a3a896dcbc5abb81
-add <temtopeayobami@gmail.com>
2024-09-10 08:49:46 +0100

    Wokring on my Post

--- server.js
+++ server.js
@@ -22,20 +22,40 @@ app.get('/about', (req, res) => {
 
 
 let posts = [
-    {id:1, title: "Post one"},
-    {id:2, title: "Post two"},
-    {id:3, title: "Post three"},
+    {id: 1, title: "Post One"},
+
+    {id: 2, title: "Post Two"},
+
+    {id: 3, title: "Post Three"},
+
+    {id: 4, title: "Post Four"},
 
 ]
 
 
 app.get('/api/posts/',  (req, res)=>{
-    res.json(posts)
-})
+
+    const limit = parseInt(req.query.limit);
+
+    if(!isNaN(limit) && limit > 0) {
+        return res.status(200).json(posts.slice(0, limit));
+    }
+        res.json(posts);
+          
+});
 
 app.get('/api/posts/:id',  (req, res)=>{
     const id = parseInt(req.params.id);
-    res.json(posts.filter((post) => post.id === id));
+    const post = posts.find((post) => post.id === id);
+
+    if(!post){
+        res.status(400).json({mes: `Post with the id of ${id} does not exist`});
+    }else{
+        res.status(200).json(post)
+    }
+
+
+    //res.json(posts.filter((post) => post.id === id));
 })
    
 
